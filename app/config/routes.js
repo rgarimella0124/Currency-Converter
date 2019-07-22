ed3c4fbef5564385b3562f77fc0ce728a0e796ed
Ram Simran Garimella <you@example.com>
2019-07-22 22:54:19 +0530

    Project Complete

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'react-native';
-import { createStackNavigator ,createAppContainer } from 'react-navigation';
+import { StatusBar } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import Home from '../screens/Home';
-import CurrencyList from '../screens/CurrencyList';
-import Options from '../screens/Options';
-import Themes from '../screens/Themes';
+import Home from "../screens/Home";
+import CurrencyList from "../screens/CurrencyList";
+import Options from "../screens/Options";
+import Themes from "../screens/Themes";
 
 const HomeStack = createStackNavigator(
   {
@@ -12,25 +12,26 @@ const HomeStack = createStackNavigator(
       screen: Home,
       navigationOptions: {
         header: () => null,
-        headerTitle: 'Home',
-      },
+        headerTitle: "Home"
+      }
     },
     Options: {
       screen: Options,
       navigationOptions: {
-        headerTitle: 'Options',
-      },
+        headerTitle: "Options"
+      }
     },
     Themes: {
       screen: Themes,
       navigationOptions: {
-        headerTitle: 'Themes',
-      },
-    },
+        headerTitle: "Themes"
+      }
+    }
   },
   {
-    headerMode: 'screen',
-  },
+    mode: "modal",
+    headerMode: "screen"
+  }
 );
 
 const HomeStackontainer = createAppContainer(HomeStack);
@@ -39,29 +40,29 @@ const CurrencyListStack = createStackNavigator({
   CurrencyList: {
     screen: CurrencyList,
     navigationOptions: ({ navigation }) => ({
-      headerTitle: navigation.state.params.title,
-    }),
-  },
+      headerTitle: navigation.state.params.title
+    })
+  }
 });
 
 const CurrencyListStackContainer = createAppContainer(CurrencyListStack);
 
-const MainStackNavigator= createStackNavigator(
+const MainStackNavigator = createStackNavigator(
   {
     Home: {
-      screen: HomeStackontainer,
+      screen: HomeStackontainer
     },
     CurrencyList: {
-      screen: CurrencyListStackContainer,
-    },
+      screen: CurrencyListStackContainer
+    }
   },
   {
-    mode: 'modal',
-    headerMode: 'none',
-    cardStyle: { paddingTop: StatusBar.currentHeight },
-  },
+    mode: "modal",
+    headerMode: "none",
+    cardStyle: { paddingTop: StatusBar.currentHeight }
+  }
 );
 
 const MainStackNavigatorContainer = createAppContainer(MainStackNavigator);
 
-export default  MainStackNavigatorContainer
+export default MainStackNavigatorContainer;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -9,8 +9,9 @@ import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Button';
 import { LastConverted } from '../components/Text';
 import { Header } from '../components/Header';
+import { connectAlert } from '../components/Alert';
 
-import { changeCurrencyAmount, swapCurrency } from '../actions/currencies';
+import { changeCurrencyAmount, swapCurrency, getInitialConversion } from '../actions/currencies';
 
 class Home extends Component {
   static propTypes = {
@@ -23,7 +24,21 @@ class Home extends Component {
     lastConvertedDate: PropTypes.object,
     isFetching: PropTypes.bool,
     primaryColor: PropTypes.string,
+    currencyError: PropTypes.string,
+    alertWithType: PropTypes.func,
   };
+
+  componentWillMount() {
+    const { dispatch } = this.props;
+    dispatch(getInitialConversion());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { currencyError, alertWithType } = this.props;
+    if (nextProps.currencyError && !currencyError) {
+      alertWithType('error', 'Error', nextProps.currencyError);
+    }
+  }
 
   handleChangeText = (text) => {
     const { dispatch } = this.props;
@@ -113,7 +128,8 @@ const mapStateToProps = (state) => {
     lastConvertedDate: conversionSelector.date ? new Date(conversionSelector.date) : new Date(),
     isFetching: conversionSelector.isFetching,
     primaryColor: state.theme.primaryColor,
+    currencyError: state.currencies.error,
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
